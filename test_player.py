5f25943742d9d262ba15184d54750b4785e3fa85
Jason Wiker <jason@vivametrica.com>
2020-08-08 11:33:25 -0600

    Add failing test for rotate boat

--- test_player.py
+++ test_player.py
@@ -2,18 +2,23 @@ from player import *
 
 def test_move_boat_right():
     player = Player()
-    assert player.move_boat_right() == ("B", "1")
+    assert player.move_boat_right() == ("B", "1", "x")
 
 def test_move_boat_left():
     player = Player()
     player.move_boat_right()
-    assert player.move_boat_left() == ("A", "1")
+    assert player.move_boat_left() == ("A", "1", "x")
 
 def test_move_boat_down():
     player = Player()
-    assert player.move_boat_down() == ("A", "2")
+    assert player.move_boat_down() == ("A", "2", "x")
 
 def test_move_boat_up():
     player = Player()
     player.move_boat_down()
-    assert player.move_boat_up() == ("A", "1")
+    assert player.move_boat_up() == ("A", "1", "x")
+
+def test_rotate_boat():
+    player = Player()
+    player.rotate_boat()
+    assert player.rotate_boat() == ("A", "1", "y")
