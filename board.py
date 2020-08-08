6e101f4f8b245ea73d56647e346a5935795c837d
Jason Wiker <jason@vivametrica.com>
2020-08-08 15:57:01 -0600

    Pass test for place missile

--- board.py
+++ board.py
@@ -21,7 +21,7 @@ class Board:
                 board[i][j] = "*"
         return board
 
-    def place_boat(self,x,y,rotation):
+    def place_item(self,x,y,rotation,item):
         """
         Places a boat on the board
 
@@ -29,17 +29,32 @@ class Board:
             x: x value of the boat origin. Should be of ["A","B","C","D","E","F","G","H"]
             y: y value of the boat origin. Should be of ["1","2","3","4","5","6","7","8"]
             rotation: rotation of boat. Should be of ["x", "y"]
+            item: value of what is to be put.
         Returns:
             Dictionary to represent board state with boat
         """
         xIndex = self.x_col.index(x)
         yIndex = self.y_col.index(y)
         if rotation == "x":
-            self.board[x][y] = "🚢"
-            self.board[self.x_col[xIndex+1]][y] = "🚢"
-            self.board[self.x_col[xIndex+2]][y] = "🚢"
+            self.board[x][y] = item
+            self.board[self.x_col[xIndex+1]][y] = item
+            self.board[self.x_col[xIndex+2]][y] = item
         if rotation == "y":
-            self.board[x][y] = "🚢"
-            self.board[x][self.y_col[yIndex+1]] = "🚢"
-            self.board[x][self.y_col[yIndex+2]] = "🚢"
+            self.board[x][y] = item
+            self.board[x][self.y_col[yIndex+1]] = item
+            self.board[x][self.y_col[yIndex+2]] = item
+        return self.board
+
+    def place_missile(self,x,y,item):
+        """
+        Places a boat on the board
+
+        Args:
+            x: x value of the missile. Should be of ["A","B","C","D","E","F","G","H"]
+            y: y value of the missile. Should be of ["1","2","3","4","5","6","7","8"]
+            item: value of what is to be put.
+        Returns:
+            Dictionary to represent board state with missiles
+        """
+        self.board[x][y] = item
         return self.board
