0eb40f889f56f7aebe8bb760bd0ba3e2019de0ad
Jason Wiker <jason@vivametrica.com>
2020-08-07 20:24:08 -0600

    Add test for render board

--- game.py
+++ game.py
@@ -4,5 +4,7 @@ class Game:
     def __init__(self):
         self.player1_board = Board()
         self.player2_board = Board()
+        print(self.player2_board.board)
 
-
+    def render_board(self):
+        return None
